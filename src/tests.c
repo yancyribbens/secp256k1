8439ce1719d3d68e8c2a1d3239c9f2e3c683832f
Jonas Nick <jonasd.nick@gmail.com>
2020-03-06 19:41:09 +0000

    f improve nonce_function_bip340 tests

--- src/tests.c
+++ src/tests.c
@@ -466,44 +466,73 @@ void test_sha256_eq(secp256k1_sha256 *sha1, secp256k1_sha256 *sha2) {
     CHECK(memcmp(buf, buf2, 32) == 0);
 }
 
+/* Checks that a bit flip in the n_flip-th argument (that has n_bytes many
+ * bytes) changes the hash function
+ */
+void nonce_function_bip340_bitflip(unsigned char **args, size_t n_flip, size_t n_bytes) {
+    unsigned char nonces[2][32];
+    CHECK(nonce_function_bip340(nonces[0], args[0], args[1], args[2], args[3], args[4], 0) == 1);
+    secp256k1_rand_flip(args[n_flip], n_bytes);
+    CHECK(nonce_function_bip340(nonces[1], args[0], args[1], args[2], args[3], args[4], 0) == 1);
+    CHECK(memcmp(nonces[0], nonces[1], 32) != 0);
+}
+
 void run_nonce_function_bip340_tests(void) {
-    char tag[12] = "BIP340/nonce";
-    char aux_tag[10] = "BIP340/aux";
-    char algo16[16] = "BIP340/nonce0000";
+    unsigned char tag[12] = "BIP340/nonce";
+    unsigned char aux_tag[10] = "BIP340/aux";
+    unsigned char algo16[16] = "BIP340/nonce0000";
     secp256k1_sha256 sha;
     secp256k1_sha256 sha_optimized;
-    unsigned char nonces[2][32];
+    unsigned char nonce[32];
     unsigned char msg[32];
     unsigned char key[32];
     unsigned char pk[32];
+    unsigned char aux_rand[32];
+    unsigned char *args[5];
 
     /* Check that hash initialized by
      * secp256k1_nonce_function_bip340_sha256_tagged has the expected
      * state. */
-    secp256k1_sha256_initialize_tagged(&sha, (unsigned char *) tag, sizeof(tag));
+    secp256k1_sha256_initialize_tagged(&sha, tag, sizeof(tag));
     secp256k1_nonce_function_bip340_sha256_tagged(&sha_optimized);
     test_sha256_eq(&sha, &sha_optimized);
 
    /* Check that hash initialized by
     * secp256k1_nonce_function_bip340_sha256_tagged_aux has the expected
     * state. */
-    secp256k1_sha256_initialize_tagged(&sha, (unsigned char *) aux_tag, sizeof(aux_tag));
+    secp256k1_sha256_initialize_tagged(&sha, aux_tag, sizeof(aux_tag));
     secp256k1_nonce_function_bip340_sha256_tagged_aux(&sha_optimized);
     test_sha256_eq(&sha, &sha_optimized);
 
-    /* Check that different choices of the algo16 argument result in different
-     * hashes. */
-    memset(msg, 0, sizeof(msg));
-    memset(key, 1, sizeof(key));
-    memset(pk, 2, sizeof(pk));
-    CHECK(nonce_function_bip340(nonces[0], msg, key, pk, NULL, NULL, 0) == 0);
-    CHECK(nonce_function_bip340(nonces[0], msg, key, pk, (unsigned char *) "BIPSchnorrDerive", NULL, 0));
-    CHECK(nonce_function_bip340(nonces[1], msg, key, pk, (unsigned char *) "something16chars", NULL, 0));
-    CHECK(memcmp(nonces[1], nonces[0], sizeof(nonces[1])) != 0);
+    secp256k1_rand256(msg);
+    secp256k1_rand256(key);
+    secp256k1_rand256(pk);
+    secp256k1_rand256(aux_rand);
+
+    /* Check that a bitflip in an argument results in different nonces. */
+    args[0] = msg;
+    args[1] = key;
+    args[2] = pk;
+    args[3] = algo16;
+    args[4] = aux_rand;
+    nonce_function_bip340_bitflip(args, 0, 32);
+    nonce_function_bip340_bitflip(args, 1, 32);
+    nonce_function_bip340_bitflip(args, 2, 32);
+    /* Flip algo16 special case "BIP340/nonce0000" */
+    nonce_function_bip340_bitflip(args, 3, 16);
+    /* Flip algo16 again */
+    nonce_function_bip340_bitflip(args, 3, 16);
+    nonce_function_bip340_bitflip(args, 4, 32);
+
+   /* NULL algo16 is disallowed */
+    CHECK(nonce_function_bip340(nonce, msg, key, pk, NULL, NULL, 0) == 0);
+
+    /* NULL aux_rand argument is allowed. */
+    CHECK(nonce_function_bip340(nonce, msg, key, pk, algo16, NULL, 0) == 1);
 
     /* Check that counter != 0 makes nonce function fail. */
-    CHECK(nonce_function_bip340(nonces[0], msg, key, pk, (unsigned char *) algo16, NULL, 0) == 1);
-    CHECK(nonce_function_bip340(nonces[0], msg, key, pk, (unsigned char *) algo16, NULL, 1) == 0);
+    CHECK(nonce_function_bip340(nonce, msg, key, pk, algo16, NULL, 0) == 1);
+    CHECK(nonce_function_bip340(nonce, msg, key, pk, algo16, NULL, 1) == 0);
 }
 
 void run_hmac_sha256_tests(void) {
